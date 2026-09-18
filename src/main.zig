f4f142d5c11f5348e8f5b025586e23842af10b9e
edwarble <ed@edwardbl.com>
2026-09-18 14:49:17 -0400

    Forward pass through attention

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
 const infer = @import("infer");
 
+// Llama3 - https://github.com/meta-llama/llama3/blob/main/llama/model.py
+// safetensor spec - https://github.com/safetensors/safetensors, https://huggingface.co/docs/safetensors/en/index
+// HF transformers model support: // https://github.com/huggingface/transformers/blob/main/src/transformers/models/auto/modeling_auto.py
+// BF16 hardware support - https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/AArch64/AArch64Processors.td
+// SIMD/etc - https://mitchellh.com/writing/everyone-should-know-simd, https://www.ziglang.in/learn/how-to/simd-sum/
+// https://developer.apple.com/documentation/accelerate, https://arxiv.org/html/2606.25426v1
+// https://developer.apple.com/metal/, https://developer.apple.com/metal/cpp/, https://opensource.apple.com/projects/mlx/, https://github.com/ml-explore/mlx-c
+
+// Tokenizer structs - merge to one?
 const PreTokenizer = struct {
     pre_tokenizer: struct {
         type: []const u8,
@@ -44,6 +53,13 @@ const TokenizerVocab = struct {
     },
 };
 
+// .safetensors header structs
+const TensorInfo = struct {
+    dtype: []const u8,
+    shape: []const u64,
+    data_offsets: [2]u64,
+};
+
 pub fn loadJson(
     comptime T: ?type,
     io: std.Io,
@@ -77,6 +93,53 @@ pub fn loadJson(
     return parsed_json;
 }
 
+pub fn mmapReadWeights(
+    arena: std.mem.Allocator,
+    key: []const u8,
+    tensor_info: std.json.ArrayHashMap(TensorInfo),
+    tensor_data: []const u8,
+) []f32 {
+    const info = tensor_info.map.get(input_layernorm_key).?;
+    std.debug.assert(std.mem.eql(u8, info.dtype, "BF16"));
+    const dim = info.shape[info.shape.len - 1];
+    const data = tensor_data[info.data_offsets[0] .. info.data_offsets[1]];
+    const weights = try arena.alloc(f32, dim);
+    for (0..dim) |i| {
+        const bf16_bytes = std.mem.readInt(u16, data[2 * i ..][0..2], .little);
+        weights[i] = @bitCast(@as(u32, bf16_bytes) << 16);
+    }
+
+    return weights;
+}
+
+pub fn simdDotProduct(
+    comptime T: type
+    x: []const T,
+    y: []const T
+) !T {
+    if (x.len != y.len) return error.InvalidValue;
+    const limit = x.len;
+
+    const lanes = std.simd.suggestVectorLength(T);
+    const V = @Vector(lanes, T);
+
+    var k: usize = 0;
+    var a: V = @splat(0);
+    while (k + lanes <= limit) : (k += lanes) {
+        const d1: V = x[k..][0..lanes].*;
+        const d2: V = x[k..][0..lanes].*;
+        a = @mulAdd(V, d1, d2, a); // equivalent to a += d1 * d2 but with one FMA instruction when hardware support available
+    }
+    var sum: T = @reduce(.Add, a);
+    while (k < limit) : (k += 1) {
+        const d1 = x[k];
+        const d2 = y[k];
+        sum = @mulAdd(T, d1, d2, sum);
+    }
+
+    return sum;
+}
+
 pub fn main(init: std.process.Init) !void {
     // .safetensors format
     // 8 bytes: N, an unsigned little-endian 64-bit integer, containing the size of the header
@@ -98,7 +161,6 @@ pub fn main(init: std.process.Init) !void {
     //     "__metadata__": { ... }
     // }
     // where dtypes is one of "F64","F32","F16","BF16","I64","I32","I16","I8","U8","BOOL"
-    // ref. https://github.com/safetensors/safetensors, https://huggingface.co/docs/safetensors/en/index
 
     // model path
     const model_dir = "models/Llama-3.2-1B-Instruct";
@@ -116,6 +178,10 @@ pub fn main(init: std.process.Init) !void {
     const config_path = model_dir ++ "/" ++ config_filename;
     const config_json = try loadJson(std.json.Value, init.io, cwd, arena, config_path, .{});
 
+    // load config params
+    const rms_norm_eps: f32 = config_json.object.get("rms_norm_eps").?;
+    const hidden_size: usize = config_json.object.get("hidden_size").?;
+
     // parse generation_config.json
     const generation_config_path = model_dir ++ "/" ++ generation_config_filename;
     const generation_config_json = try loadJson(std.json.Value, init.io, cwd, arena, generation_config_path, .{});
@@ -132,7 +198,7 @@ pub fn main(init: std.process.Init) !void {
     const merges_json = try loadJson(TokenizerMerges, init.io, cwd, arena, tokenizer_path, .{ .ignore_unknown_fields = true });
     const vocab_json = try loadJson(TokenizerVocab, init.io, cwd, arena, tokenizer_path, .{ .ignore_unknown_fields = true });
 
-    // swap vocab bytes with BBPE
+    // load vocab, swap utf-8 codepoints per BBPE
     var vocab_bbpe_swap: std.json.ArrayHashMap(u32) = .{};
     var vocab_raw_iterator = vocab_json.model.vocab.map.iterator();
     while (vocab_raw_iterator.next()) |tok| {
@@ -151,6 +217,7 @@ pub fn main(init: std.process.Init) !void {
          return error.InvalidValue;
      };
 
+    // load merges, swap token utf-8 codepoints per BBPE
     var merge_rules: std.json.ArrayHashMap(u32) = .{};
     var merge_ranks: std.json.ArrayHashMap(u32) = .{};
     var merge_buf: [2048]u8 = undefined;
@@ -282,6 +349,7 @@ pub fn main(init: std.process.Init) !void {
             pos += len;
         }
     }
+    const sequence_len = token_ids.len;
 
     // open .safetensors file with mmap
     const model_path = model_dir ++ "/" ++ model_filename;
@@ -317,66 +385,181 @@ pub fn main(init: std.process.Init) !void {
         header,
         .{}
     );
+    var tensor_info: std.json.ArrayHashMap(u32) = .{};
+    var tensor_iterator = header_json.object.iterator();
+    while (tensor_iterator.next()) |t| {
+        const tname = t.key_ptr.*;
+        if (std.mem.eql(u8, tname, "__metadata__")) continue;
+
+        const info = try std.json.parseFromValueLeaky(
+            TensorInfo,
+            arena,
+            t.value_ptr.*,
+            .{},
+        );
+
+        try tensor_info.map.put(arena, tname, info);
+    }
     const data_offset: usize = hls_u64 + hsize;
     const tensor_data = mmbuf[data_offset..];
 
-    // embed tokens data location
-    const embed_tokens = header_json.object.get("model.embed_tokens.weight").?;
-    const embed_tokens_dtype = embed_tokens.object.get("dtype").?.string;
+    // embed tokens tensors
+    const embed_tokens = tensor_info.map.get("model.embed_tokens.weight").?;
+    const embed_tokens_dim = embed_tokens.shape[embed_tokens.shape.len - 1];
+    std.debug.assert(embed_tokens_dim == hidden_size);
 
-    const embed_tokens_data_offsets = embed_tokens.object.get("data_offsets").?.array.items;
-    const embed_tokens_start: usize = @intCast(embed_tokens_data_offsets[0].integer);
-    const embed_tokens_end: usize = @intCast(embed_tokens_data_offsets[1].integer);
+    std.debug.assert(std.mem.eql(u8, embed_tokens.dtype, "BF16"));
+    var bytes_per_row: usize = hidden_size * 2;
 
-    const embed_tokens_shape = embed_tokens.object.get("shape").?.array.items;
-    const embed_tokens_num: usize = @intCast(embed_tokens_shape[0].integer);
-    const embed_tokens_dim: usize = @intCast(embed_tokens_shape[1].integer);
+    const embed_tokens_data = tensor_data[embed_tokens.data_offsets[0] .. embed_tokens.data_offsets[1]];
 
-    var bytes_per_row: usize = embed_tokens_dim * 2; // fix default handling
-    if (std.mem.eql(u8, embed_tokens_dtype, "BF16")) {
-        bytes_per_row = embed_tokens_dim * 2;
-    }
-    else {
-        return error.NotImplemented;
-    }
-
-    const embed_tokens_data = tensor_data[embed_tokens_start .. embed_tokens_end];
-
-    // lookup input embeddings
-    const embeddings = try arena.alloc(f32, token_ids.items.len * embed_tokens_dim);
+    // input embedding via lookup
+    const activations = try arena.alloc(f32, token_ids.items.len * hidden_size);
     for (token_ids.items, 0..) |tid, tpos| {
         const emb_offset = @as(usize, tid) * bytes_per_row;
         const embedding_bytes = embed_tokens_data[emb_offset..][0..bytes_per_row];
 
-        for (0..embed_tokens_dim) |vpos| {
-            // https://en.wikipedia.org/wiki/Single-precision_floating-point_format
-            // f32 format: | 1 sign bit | 8 exponent bits | 23 fraction/precision bits |
-            // https://en.wikipedia.org/wiki/Bfloat16_floating-point_format
-            // bf16 format: | 1 sign bit | 8 exponent bits | 7 fraction/precision bits |
-            // no native support in Zig for b16, hence: (a) read as u16, (b) convert to u32, (c) left-shift by 16 bits, (d) convert to f32
-            const bits = std.mem.readInt(u16, embedding_bytes[2 * vpos ..][0..2], .little);
-            embeddings[tpos * embed_tokens_dim + vpos] = @bitCast(@as(u32, bits) << 16);
+        // https://en.wikipedia.org/wiki/Single-precision_floating-point_format
+        // f32 format: | 1 sign bit | 8 exponent bits | 23 fraction/precision bits |
+        // https://en.wikipedia.org/wiki/Bfloat16_floating-point_format
+        // bf16 format: | 1 sign bit | 8 exponent bits | 7 fraction/precision bits |
+        // steps: (a) read as u16, (b) convert to u32, (c) left-shift by 16 bits, (d) convert to f32 (with implicit typing for @bitCast)
+        for (0..hidden_size) |vpos| {
+            const bf16_bytes = std.mem.readInt(u16, embedding_bytes[2 * vpos ..][0..2], .little);
+            activations[tpos * hidden_size + vpos] = @bitCast(@as(u32, bf16_bytes) << 16);
         }
     }
 
     // forward pass
+    // prefill
     const num_hidden_layers: usize = @intCast(config_json.object.get("num_hidden_layers").?.integer);
+    const layer_buf: [2048]u8 = undefined;
     for (0..num_hidden_layers) |i| {
         std.debug.print("layer-{d} ", .{i});
+
+        // input RMSNorm
+        // RMSNorm(a_i) = (1/RMS(a)) * (a_i * g_i) and RMS(a) = \sqrt((rms_norm_eps + (1/hidden_size) * \sum_{i=1}^{hidden_size} a_i^2))
+
+        // load learnable gain parameters g
+        const input_layernorm_key = try std.fmt.bufPrint(&layer_buf, "model.layers.{d}.input_layernorm.weight", .{ i });
+        std.debug.assert(tensor_info.map.get(input_layernorm_key).?.shape[info.shape.len - 1] == hidden_size);
+        const gain_params = try mmapReadWeights(input_layernorm_key, tensor_info, tensor_data);
+
+        // RMS norm calc (replace with function)
+        const lanes = std.simd.suggestVectorLength(f32);
+        const V = @Vector(lanes, f32);
+        for (0..sequence_len) |j| {
+            // dot product
+            const sum: f32 = try simdDotProduct(f32, activations[j*hidden_size..][0..hidden_size], activations[j*hidden_size..][0..hidden_size]);
+            const rms_inv = 1 / @sqrt( ( rms_norm_eps + (1 / hidden_size) * sum ) );
+
+            // (1/RMS(a)) * a_i * g_i, move to simdVectorHadamard() function?
+            var k: usize = 0;
+            var accumulator: V = @splat(0);
+            const rms_inv_vec: V = @splat(rms_inv);
+            while (k + lanes <= hidden_size) : (k += lanes) {
+                const g: V = gain_params[k..][0..lanes].*;
+                const a: V = activations[j * hidden_size + k ..][0..lanes].*;
+                const out = rms_inv_vec * g * a;
+                activations[j * hidden_size + k ..][0..lanes].* = out;
+            }
+            while (k < hidden_size) : (k += 1) {
+                const g = gain_params[k];
+                const a = activations[j * hidden_size + k];
+                const out = rms_inv * g * a;
+                activations[j * hidden_size + k] = out;
+            }
+        }
+
+        // causal self-attention
+        const num_attention_heads: usize = @intCast(config_json.object.get("num_attention_heads").?.integer);
+        const num_key_value_heads: usize = @intCast(config_json.object.get("num_key_value_heads").?.integer);
+        const head_dim: usize = @intCast(config_json.object.get("head_dim").?.integer);
+
+        const qproj_key = try std.fmt.bufPrint(&layer_buf, "model.layers.{d}.self_attn.q_proj.weight", .{ i });
+        std.debug.assert((tensor_info.map.get(qproj_key).?.shape[0] / num_attention_heads) == head_dim);
+        std.debug.assert(tensor_info.map.get(qproj_key).?.shape[tensor_info.map.get(qproj_key).?.shape.len - 1] == hidden_size);
+        const qproj_weights = try mmapReadWeights(qproj_key, tensor_info, tensor_data);
+
+        const kproj_key = try std.fmt.bufPrint(&layer_buf, "model.layers.{d}.self_attn.k_proj.weight", .{ i });
+        std.debug.assert((tensor_info.map.get(kproj_key).?.shape[0] / num_key_value_heads) == head_dim);
+        std.debug.assert(tensor_info.map.get(kproj_key).?.shape[tensor_info.map.get(kproj_key).?.shape.len - 1] == hidden_size);
+        const kproj_weights = try mmapReadWeights(kproj_key, tensor_info, tensor_data);
+
+        const vproj_key = try std.fmt.bufPrint(&layer_buf, "model.layers.{d}.self_attn.v_proj.weight", .{ i });
+        std.debug.assert((tensor_info.map.get(vproj_key).?.shape[0] / num_key_value_heads) == num_attn_keys);
+        std.debug.assert(tensor_info.map.get(vproj_key).?.shape[tensor_info.map.get(vproj_key).?.shape.len - 1] == hidden_size);
+        const vproj_weights = try mmapReadWeights(vproj_key, tensor_info, tensor_data);
+
+        const oproj_key = try std.fmt.bufPrint(&layer_buf, "model.layers.{d}.self_attn.o_proj.weight", .{ i });
+        std.debug.assert((tensor_info.map.get(oproj_key).?.shape[0] / num_attention_heads) == num_attn_queries);
+        std.debug.assert(tensor_info.map.get(oproj_key).?.shape[tensor_info.map.get(oproj_key).?.shape.len - 1] == hidden_size);
+        const oproj_weights = try mmapReadWeights(oproj_key, tensor_info, tensor_data);
+
+        for (0..num_attention_heads) |i| {
+            // matmul Q * A
+
+            for (0..num_key_value_heads) |j| {
+                // matmul K * A, cache
+                // matmul V * A, cache
+                ...
+            }
+        }
+
+
+
+        // attention (GQA-8) with RoPE + KV caching, + residual
+        // post-attention RMS norm
+        // FFN with SwiGLU, + residual
     }
 
-    // output embedding
+    // RMS norm
+    // logits = outvec * E^T
+    // sample next token from logits
+
+    // decode
 
     // debug prints and discards
     std.debug.print("\n\n___rendered input:\n{s}", .{rendered_input});
-    // std.debug.print("---header---\n{f}\n", .{std.json.fmt(header_json, .{ .whitespace = .indent_4 })});
-    std.debug.print("\n___token sequence:\n", .{});
-    for (token_ids.items, 0..) |tid, tpos| {
-        const row = embeddings[tpos * embed_tokens_dim ..][0..embed_tokens_dim];
-        std.debug.print("token {d}: {any}\n", .{ tid, row });
-    }
+    std.debug.print("---header---\n{f}\n", .{std.json.fmt(header_json, .{ .whitespace = .indent_4 })});
+    // std.debug.print("\n___token sequence:\n", .{});
+    // for (token_ids.items, 0..) |tid, tpos| {
+    //     const row = embeddings[tpos * hidden_size ..][0..hidden_size];
+    //     std.debug.print("token {d}: {any}\n", .{ tid, row });
+    // }
     _ = embed_tokens_num;
     _ = generation_config_json; // stop tokens
     _ = tokenizer_config_json;
     _ = tokenizer_json;
+
+    // https://github.com/meta-llama/llama3/blob/main/llama/model.py
+    // class TransformerBlock(nn.Module):
+    //     def __init__(...):
+    //         ...
+    //         self.feed_forward = FeedForward(
+    //             dim=args.dim,
+    //             hidden_dim=4 * args.dim,
+    //             multiple_of=args.multiple_of,
+    //             ffn_dim_multiplier=args.ffn_dim_multiplier,
+    //         )
+    //         ...
+    //         self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+    //         self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+    //     def forward(...):
+    //         h = x + self.attention(self.attention_norm(x), start_pos, freqs_cis, mask)
+    //         out = h + self.feed_forward(self.ffn_norm(h))
+    //         return out
+    //
+    // class Transformer(nn.Module):
+    //     ...
+    //     def forward(...):
+    //         _bsz, seqlen = tokens.shape
+    //         h = self.tok_embeddings(tokens)
+    //         ... # compute mask and freqs_cis (cosine/sine freqs for RoPE)
+    //         for layer in self.layers:
+    //             h = layer(h, start_pos, freqs_cis, mask)
+    //         h = self.norm(h)
+    //         output = self.output(h).float()
+    //         return output
+
 }
